0e0b44cbd75f5f8d74cf5e9d0a289d5d6c6ae7ef
GannaKov <zlatta2000@gmail.com>
2024-04-20 13:32:07 +0200

    usefulLinks

--- src/components/AddForms/FormFourth.jsx
+++ src/components/AddForms/FormFourth.jsx
@@ -14,7 +14,10 @@ import DeleteIcon from "@mui/icons-material/Delete";
 
 const FormFourth = () => {
   const [expenses, setExpenses] = useState([]);
+  const [usefulLinks, setUsefulLinks] = useState([]);
   const initialValues = {
+    topic: "",
+    link: "",
     item: "",
     amount: null,
     advices: "",
@@ -23,21 +26,40 @@ const FormFourth = () => {
     initialValues,
     onSubmit: (values) => {
       //console.log("Submitted values:", values);
-      console.log("Submitted values:", { expenses, advices: values.advices });
+      console.log("Submitted values:", {
+        expenses,
+        advices: values.advices,
+        useful_links: usefulLinks,
+      });
     },
   });
   const onExpenseAddBtnClick = () => {
-    console.log(formik.values);
     const { item, amount } = formik.values;
     if (item.trim() !== "" && amount.trim() !== null && amount !== "") {
       setExpenses((prevExpenses) => [...prevExpenses, { item, amount }]);
+
       formik.setValues({ ...formik.values, item: "", amount: null });
     } else {
-      alert("Empty Fields");
+      alert("Please enter a valid item and amount");
     }
   };
-  const handleRemove = (index) => {
-    setExpenses((prevExpenses) => prevExpenses.filter((_, i) => i !== index));
+  const onLinkAddBtnClick = () => {
+    console.log(formik.values);
+    const { topic, link } = formik.values;
+    if (topic.trim() !== "" && link.trim() !== "") {
+      setUsefulLinks((prevLinks) => [...prevLinks, { topic, link }]);
+      formik.setValues({ ...formik.values, topic: "", link: "" });
+    } else {
+      alert("Please enter a valid topic and link");
+    }
+  };
+  const handleRemove = (index, subject) => {
+    if (subject === "expense") {
+      setExpenses((prevExpenses) => prevExpenses.filter((_, i) => i !== index));
+    }
+    if (subject === "link") {
+      setUsefulLinks((prevLinks) => prevLinks.filter((_, i) => i !== index));
+    }
   };
 
   return (
@@ -69,42 +91,91 @@ const FormFourth = () => {
         >
           Add
         </Button>
+        {expenses.length > 0 && (
+          <Box mt={4}>
+            <Typography variant="h6">Added Expenses</Typography>
+            <List>
+              {expenses.map((expense, index) => (
+                <ListItem key={index}>
+                  <ListItemText
+                    sx={{ display: "flex", gap: "1rem" }}
+                    primary={`${expense.item} :  ${expense.amount}`}
+                  />
+                  <IconButton
+                    edge="end"
+                    aria-label="delete"
+                    onClick={() => handleRemove(index, "expense")}
+                  >
+                    <DeleteIcon />
+                  </IconButton>
+                </ListItem>
+              ))}
+            </List>
+          </Box>
+        )}
         <div>
           <TextField
             id="advices"
             label="Advices"
             multiline
-            maxRows={4}
+            minRows={3}
+            maxRows={6}
             onChange={formik.handleChange}
             fullWidth
           />
         </div>
-
+        {/* ------ Links ------- */}
+        <TextField
+          id="topic"
+          name="topic"
+          label="Topic"
+          value={formik.values.topic}
+          onChange={formik.handleChange}
+          variant="outlined"
+          fullWidth
+        />
+        <TextField
+          id="link"
+          name="link"
+          label="Link"
+          value={formik.values.link}
+          onChange={formik.handleChange}
+          variant="outlined"
+          fullWidth
+        />
+        <Button
+          type="button"
+          variant="contained"
+          sx={{ mt: 2 }}
+          onClick={onLinkAddBtnClick}
+        >
+          Add
+        </Button>
+        {usefulLinks.length > 0 && (
+          <Box mt={4}>
+            <Typography variant="h6">Added Links</Typography>
+            <List>
+              {usefulLinks.map((link, index) => (
+                <ListItem key={index}>
+                  <ListItemText
+                    sx={{ display: "flex", gap: "1rem" }}
+                    primary={`${link.topic} :  ${link.link}`}
+                  />
+                  <IconButton
+                    edge="end"
+                    aria-label="delete"
+                    onClick={() => handleRemove(index, "link")}
+                  >
+                    <DeleteIcon />
+                  </IconButton>
+                </ListItem>
+              ))}
+            </List>
+          </Box>
+        )}
+        {/* ------ end Links ------- */}
         <Button type="submit">Submit</Button>
       </form>
-
-      {expenses.length > 0 && (
-        <Box mt={4}>
-          <Typography variant="h6">Добавленные расходы:</Typography>
-          <List>
-            {expenses.map((expense, index) => (
-              <ListItem key={index}>
-                <ListItemText
-                  primary={expense.item}
-                  secondary={`Amount: ${expense.amount}`}
-                />
-                <IconButton
-                  edge="end"
-                  aria-label="delete"
-                  onClick={() => handleRemove(index)}
-                >
-                  <DeleteIcon />
-                </IconButton>
-              </ListItem>
-            ))}
-          </List>
-        </Box>
-      )}
     </div>
   );
 };
