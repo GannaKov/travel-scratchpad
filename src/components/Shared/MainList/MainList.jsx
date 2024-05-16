391f19e03a3f6bfe9340dd9ea3e90b79251a97e5
GannaKov <zlatta2000@gmail.com>
2024-05-16 03:26:37 +0200

    fix styles

--- src/components/Shared/MainList/MainList.jsx
+++ src/components/Shared/MainList/MainList.jsx
@@ -7,29 +7,28 @@ import styles from "./MainList.module.css";
 import { ButtonsTemplate } from "../Buttons/Buttons";
 
 const MainList = ({ tripsArr }) => {
-
   const location = useLocation();
   return (
     <ul className={styles.mainList}>
       {tripsArr.map((trip) => (
-        <div key={trip._id} className={styles.mainItemWrp}>
-          <li className={styles.mainItem}>
-            <TripShortItem trip={trip} />
-            <Link
-              to={`${trip._id}`}
-              state={{ from: location }}
-              className={styles.mainItemLink}
+        // <div key={trip._id} className={styles.mainItemWrp}>
+        <li className={styles.mainItem} key={trip._id}>
+          <TripShortItem trip={trip} />
+          <Link
+            to={`${trip._id}`}
+            state={{ from: location }}
+            className={styles.mainItemLink}
+          >
+            <ButtonsTemplate
+              color="darkGreen"
+              size="medium"
+              variant="contained"
             >
-              <ButtonsTemplate
-                color="darkGreen"
-                size="medium"
-                variant="contained"
-              >
-                See more
-              </ButtonsTemplate>
-            </Link>
-          </li>
-        </div>
+              See more
+            </ButtonsTemplate>
+          </Link>
+        </li>
+        // </div>
       ))}
     </ul>
   );
--- src/components/TripShortItem/TripShortItem.jsx
+++ src/components/TripShortItem/TripShortItem.jsx
@@ -20,20 +20,24 @@ const TripShortItem = ({ trip }) => {
 
   return (
     <div className={styles.tripItemWrp}>
-      <div className={styles.tripOwnerAvatarWrp}>
-        <p className={styles.tripOwnerAvatarText}>{userOwnerTrip.username}</p>
+      <div className={styles.topCardWrp}>
+        {" "}
+        <div className={styles.tripOwnerAvatarWrp}>
+          <p className={styles.tripOwnerAvatarText}>{userOwnerTrip.username}</p>
+        </div>
+        <h2 className={styles.tripItemTitle}>{trip.title}</h2>
+        <StarsShow
+          rating={trip.travel_rating}
+          isReadOnly={true}
+          className={styles.tripItemRating}
+        />
+        <p className={styles.tripItemDate}>
+          {dayjs(trip.date_start).format("DD.MM.YYYY")}
+          &nbsp;&nbsp;-&nbsp;&nbsp;
+          {dayjs(trip.date_end).format("DD.MM.YYYY")}
+        </p>
       </div>
-      <h2 className={styles.tripItemTitle}>{trip.title}</h2>
-      <StarsShow
-        rating={trip.travel_rating}
-        isReadOnly={true}
-        className={styles.tripItemRating}
-      />
-      <p className={styles.tripItemDate}>
-        {dayjs(trip.date_start).format("DD.MM.YYYY")}
-        &nbsp;&nbsp;-&nbsp;&nbsp;
-        {dayjs(trip.date_end).format("DD.MM.YYYY")}
-      </p>
+
       <div className={styles.tripImageWrp}>
         {trip.main_img ? (
           <img src={trip.main_img} className={styles.tripItemImg} />
@@ -41,29 +45,33 @@ const TripShortItem = ({ trip }) => {
           <img src={TravelPlaceholder} className={styles.tripItemImg} />
         )}
       </div>
-      <div className={styles.tripChunk}>
-        <span className={styles.tripBoldText}>Purpose:&nbsp;</span>
-        {trip.purpose.map((item) => (
-          <span key={item}>{item},</span>
-        ))}
-      </div>
-      <div className={styles.tripChunk}>
-        <span className={styles.tripBoldText}>Countries:&nbsp;</span>
-        {trip.countries.map((country) => (
-          <span key={country}>{country},&nbsp;</span>
-        ))}
-      </div>
-      <div className={styles.tripChunk}>
-        <span className={styles.tripBoldText}>Destination:&nbsp;</span>
-        {trip.destination.length > 0 ? (
-          trip.destination.map((city) => <span key={city}>{city},&nbsp;</span>)
-        ) : (
-          <span>&#x1F937;</span>
-        )}
-      </div>
-      <div className={styles.tripChunk}>
-        <span className={styles.tripBoldText}>Amount:&nbsp;</span>
-        {trip.total_amount ? <span>{trip.total_amount}</span> : <p>💰</p>}
+      <div className={styles.bottomCardWrp}>
+        <div className={styles.tripChunk}>
+          <span className={styles.tripBoldText}>Purpose:&nbsp;</span>
+          {trip.purpose.map((item) => (
+            <span key={item}>{item},</span>
+          ))}
+        </div>
+        <div className={styles.tripChunk}>
+          <span className={styles.tripBoldText}>Countries:&nbsp;</span>
+          {trip.countries.map((country) => (
+            <span key={country}>{country},&nbsp;</span>
+          ))}
+        </div>
+        <div className={styles.tripChunk}>
+          <span className={styles.tripBoldText}>Destination:&nbsp;</span>
+          {trip.destination.length > 0 ? (
+            trip.destination.map((city) => (
+              <span key={city}>{city},&nbsp;</span>
+            ))
+          ) : (
+            <span>&#x1F937;</span>
+          )}
+        </div>
+        <div className={styles.tripChunk}>
+          <span className={styles.tripBoldText}>Amount:&nbsp;</span>
+          {trip.total_amount ? <span>{trip.total_amount}</span> : <p>💰</p>}
+        </div>
       </div>
     </div>
   );
